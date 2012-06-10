d54096c94991aa29b0fa43f6157bd4ceae158187
Bruno Meira <goesmeira@gmail.com>
2012-06-10 01:42:49 -0400

    Added tests

--- FlickrSmallProject/app/assets/javascripts/pages/index.js
+++ FlickrSmallProject/app/assets/javascripts/pages/index.js
@@ -7,41 +7,51 @@ $(document).ready(function() {
 });
 function bindFormEvent() {
 	var showLoad;
-	$("#searchForm").bind("ajax:beforeSend", function(jqXHR, settings) {
-		$("#imagesArea").fadeOut("slow");
-		$("#loadingArea").fadeIn("slow");
+	$(".btn").click(function() {
 		$("#imagesArea").empty();
-		showLoad = true;
-	}).bind("ajax:success", function(evt, results, status) {
-		$(results).each(function(index, element) {
-			$("#imagesArea").append(mountImageDiv(element.title, element.url));
-			if(showLoad) {
-				showLoad = false;
-				$("#loadingArea").fadeOut("slow");
-				$("#imagesArea").fadeIn("slow");
-			}
-		});
-
-		$("body").attr("page", 2);
+		returnImages(1, $("#q").val());
+		return false;
 	});
 }
 
 function returnImages(page, query) {
 	$("#loadingArea").fadeIn("slow");
-	var screen_name = 'tableless';
 	var url = '/search';
-
-	$('body').data('attr', page + 1);
-	ajax = $.getJSON(url, {
-		q : query,
-		page : page
-	}, function(results) {
-		$(results).each(function(index, element) {
-			$("#imagesArea").append(mountImageDiv(element.title, element.url));
+	var backRequests = 0;
+	var numberOfRequests = 0;
+	var valueOnSearchBox = $("#q").val();
+	while(numberOfRequests < 5) {
+		disableFields();
+		$('body').attr('page', page++);
+		ajax = $.getJSON(url, {
+			q : query,
+			page : page
+		}, function(results) {
+			$(results).each(function(index, element) {
+				$("#imagesArea").append(mountImageDiv(element.title, element.url));
+			});
+			backRequests++;
+			if(backRequests == 5) {
+				$("#loadingArea").fadeOut("slow");
+				startScroll();
+				
+				enableFields(valueOnSearchBox);
+			}
 		});
-		$("#loadingArea").fadeOut("slow");
-		startScroll();
-	});
+		numberOfRequests++;
+	}
+}
+
+function disableFields(){
+	$("#q").attr('disabled','disabled');
+	$("#q").val("Wait until all 10 images are retrieved");
+	$(".btn").attr("disabled", true);
+}
+
+function enableFields(searchText){
+	$("#q").val(searchText);
+	$("#q").removeAttr('disabled');	
+	$(".btn").attr("disabled", false);
 }
 
 function startScroll() {
@@ -49,14 +59,14 @@ function startScroll() {
 		if(($(window).scrollTop() + $(window).height() + 20) >= $(document).height()) {
 			$(window).unbind('scroll');
 			ajax.abort();
-			returnImages($('body').data('page'));
+			returnImages($('body').attr('page'), $("#q").val());
 		}
 	});
 }
 
 function mountImageDiv(title, url) {
 	html = "<div style=\"display: inline; margin:20px;\">"
-	html += "<img src=\"" + url + "\"  style=\"padding:5px;border:1px solid #021a40;\"\>"
+	html += "<img src=\"" + url + "\"  style=\"padding:5px;border:1px solid #021a40;\" title='"+title+"'\>"
 	html += "</div>"
 	return html;
 }
--- FlickrSmallProject/app/controllers/flickr_controller.rb
+++ FlickrSmallProject/app/controllers/flickr_controller.rb
@@ -4,14 +4,17 @@ class FlickrController < ApplicationController
   def search
     FlickRaw.api_key="ca351878db84a6065d181084a279c5a8"
     FlickRaw.shared_secret="bfac42d276231fd9"
-    
     if params[:q].nil? || params[:q] ==""
-      @list = flickr.photos.getRecent(:per_page=>10,:page=>params[:page])
+      if params[:page].nil?
+        @list = flickr.photos.getRecent(:per_page=>2,:page=>1)
+      else
+        @list = flickr.photos.getRecent(:per_page=>2,:page=>params[:page])
+      end
     else
       if params[:page].nil?
-        @list = flickr.photos.search(:text=>params[:q],:per_page=>10,:page=>1)
+        @list = flickr.photos.search(:text=>params[:q],:per_page=>2,:page=>1)
       else
-        @list = flickr.photos.search(:text=>params[:q],:per_page=>10,:page=>params[:page])
+        @list = flickr.photos.search(:text=>params[:q],:per_page=>2,:page=>params[:page])
       end
     end
     @elements = Array.new
@@ -23,7 +26,7 @@ class FlickrController < ApplicationController
       @elements << flick
     end
     respond_to do |format|
-      format.json { render :json => @elements.to_json}
+      format.json { render :json => @elements.to_json, :content_type => "application/json"}
     end
   end
 end
